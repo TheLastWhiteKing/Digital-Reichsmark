986ba72d9a9067ae14c08ad3161d5038379471c4
digitalreichsmark <35603756+digitalreichsmarkproject@users.noreply.github.com>
2018-01-23 23:33:00 +0100

    Genesis TX
    013c01ff0001cf02029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101a74ef4f1363531bc99126709269bfe46fcc1df7957419ef143a00d848da9e73e

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,7 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "digitalreichsmark";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff0001cf02029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101a74ef4f1363531bc99126709269bfe46fcc1df7957419ef143a00d848da9e73e";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
