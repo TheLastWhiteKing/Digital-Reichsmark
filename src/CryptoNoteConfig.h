9f1f68fa957dbb684d6b507139ab4f0948e165dd
digitalreichsmark <35603756+digitalreichsmarkproject@users.noreply.github.com>
2018-01-23 22:35:32 +0100

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -107,7 +107,7 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
   //"159.65.16.199:14880",
-  //"139.59.162.155:14880",
+  //"46.101.146.115:14880",
 };
 
 struct CheckpointData {
